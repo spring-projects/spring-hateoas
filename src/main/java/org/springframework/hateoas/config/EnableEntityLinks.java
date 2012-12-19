87537a4defaeb7b541b5df0649f83799e0892730
Oliver Gierke <info@olivergierke.de>
2012-12-19 14:32:06 +0100

    #32 - @EnableEntityLinks is now inherited and documented.

--- src/main/java/org/springframework/hateoas/config/EnableEntityLinks.java
+++ src/main/java/org/springframework/hateoas/config/EnableEntityLinks.java
@@ -15,7 +15,9 @@
  */
 package org.springframework.hateoas.config;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -32,6 +34,8 @@ import org.springframework.hateoas.core.DelegatingEntityLinks;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
+@Documented
 @Import(LinkBuilderBeanDefinitionRegistrar.class)
 public @interface EnableEntityLinks {
 
