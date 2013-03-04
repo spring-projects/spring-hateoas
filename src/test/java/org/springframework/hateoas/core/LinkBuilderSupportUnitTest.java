spring-projects/spring-hateoas

commit 84efebc7a65afd7b5decbae054af13406ccb59c2

#26, #39 - Added support for request parameters to ControllerLinkBuilder.
We now transparently handle "?foo=bar" expressions passed into ….slash(…). Added method to return UriComponentsBuilder to allow more fine grained control over the URI to be built if needed. The dummy method invocations handled to ….linkTo(…) now transparently adds the values handed in for parameters annotated with @RequestParam.

Most of the commit is highly inspired by Dietrich Schultens work at [0]. I just reworked and polished it slightly as we had too many merge conflicts due to related changes in the meantime.

[0] https://github.com/dschulten/spring-hateoas/commit/1ebdc9025a458e91e92f573692e0484ca568982b

src/main/java/org/springframework/hateoas/core/LinkBuilderSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import org.springframework.hateoas.Identifiable;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.LinkBuilder;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -60,8 +59,14 @@ public abstract class LinkBuilderSupport<T extends LinkBuilder> implements LinkB
 			return slash((Identifiable<?>) object);
 		}
 
-		String[] segments = StringUtils.tokenizeToStringArray(object.toString(), "/");
-		return createNewInstance(UriComponentsBuilder.fromUri(uriComponents.toUri()).pathSegment(segments));
+		UriComponents components = UriComponentsBuilder.fromUriString(object.toString()).build();
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUri(uriComponents.toUri());
+
+		for (String pathSegment : components.getPathSegments()) {
+			builder.pathSegment(pathSegment);
+		}
+
+		return createNewInstance(builder.query(components.getQuery()));
 	}
 
 	/*

src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -150,6 +150,15 @@ public class ControllerLinkBuilder extends LinkBuilderSupport<ControllerLinkBuil
 	@Override
 	protected ControllerLinkBuilder createNewInstance(UriComponentsBuilder builder) {
 		return new ControllerLinkBuilder(builder);
+	}
+
+	/**
+	 * Returns a {@link UriComponentsBuilder} to continue to build the already built URI in a more fine grained way.
+	 * 
+	 * @return
+	 */
+	public UriComponentsBuilder toUriComponentsBuilder() {
+		return UriComponentsBuilder.fromUri(toUri());
 	}
 
 	/**

src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilderFactory.java
@@ -16,14 +16,15 @@
 package org.springframework.hateoas.mvc;
 
 import java.lang.reflect.Method;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.core.MethodParameter;
@@ -38,6 +39,7 @@ import org.springframework.hateoas.core.MethodParameters;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UriTemplate;
 
@@ -50,8 +52,10 @@ import org.springframework.web.util.UriTemplate;
 public class ControllerLinkBuilderFactory implements MethodLinkBuilderFactory<ControllerLinkBuilder> {
 
 	private static final MappingDiscoverer DISCOVERER = new AnnotationMappingDiscoverer(RequestMapping.class);
-	private static final AnnotatedParametersParameterAccessor ACCESSOR = new AnnotatedParametersParameterAccessor(
+	private static final AnnotatedParametersParameterAccessor PATH_VARIABLE_ACCESSOR = new AnnotatedParametersParameterAccessor(
 			new AnnotationAttribute(PathVariable.class));
+	private static final AnnotatedParametersParameterAccessor REQUEST_PARAM_ACCESSOR = new AnnotatedParametersParameterAccessor(
+			new AnnotationAttribute(RequestParam.class));
 
 	private List<UriComponentsContributor> uriComponentsContributors = new ArrayList<UriComponentsContributor>();
 
@@ -107,11 +111,24 @@ public class ControllerLinkBuilderFactory implements MethodLinkBuilderFactory<Co
 			}
 		}
 
-		values.putAll(ACCESSOR.getBoundParameters(invocation));
-		URI uri = template.expand(values);
+		values.putAll(PATH_VARIABLE_ACCESSOR.getBoundParameters(invocation));
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUri(template.expand(values));
 
-		UriComponentsBuilder builder = ControllerLinkBuilder.getBuilder();
-		return new ControllerLinkBuilder(applyUriComponentsContributer(builder, invocation)).slash(uri);
+		for (Entry<String, Object> param : REQUEST_PARAM_ACCESSOR.getBoundParameters(invocation).entrySet()) {
+
+			Object value = param.getValue();
+			String key = param.getKey();
+
+			if (value instanceof Collection) {
+				for (Object element : (Collection<?>) value) {
+					builder.queryParam(key, element);
+				}
+			} else {
+				builder.queryParam(key, value);
+			}
+		}
+
+		return new ControllerLinkBuilder(applyUriComponentsContributer(builder, invocation));
 	}
 
 	/* 

src/test/java/org/springframework/hateoas/mvc/ControllerLinkBuilderUnitTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -26,11 +29,17 @@ import org.springframework.hateoas.Identifiable;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.TestUtils;
 import org.springframework.http.HttpEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
+ * Unit tests for {@link ControllerLinkBuilder}.
+ * 
  * @author Oliver Gierke
  */
 public class ControllerLinkBuilderUnitTest extends TestUtils {
@@ -124,6 +133,69 @@ public class ControllerLinkBuilderUnitTest extends TestUtils {
 		assertThat(link.getHref(), startsWith("http://somethingDifferent"));
 	}
 
+	/**
+	 * @see #26, #39
+	 */
+	@Test
+	public void addsRequestParametersHandedIntoSlashCorrectly() {
+
+		Link link = linkTo(PersonController.class).slash("?foo=bar").withSelfRel();
+
+		UriComponents components = toComponents(link);
+		assertThat(components.getQuery(), is("foo=bar"));
+	}
+
+	/**
+	 * @see #26, #39
+	 */
+	@Test
+	public void linksToMethodWithPathVariableAndRequestParams() {
+
+		Link link = linkTo(methodOn(ControllerWithMethods.class).methodForNextPage("1", 10, 5)).withSelfRel();
+
+		UriComponents components = toComponents(link);
+		assertThat(components.getPath(), is("/something/1/foo"));
+
+		MultiValueMap<String, String> queryParams = components.getQueryParams();
+		assertThat(queryParams.get("limit"), contains("5"));
+		assertThat(queryParams.get("offset"), contains("10"));
+	}
+
+	/**
+	 * @see #26, #39
+	 */
+	@Test
+	public void linksToMethodWithPathVariableAndMultiValueRequestParams() {
+
+		Link link = linkTo(
+				methodOn(ControllerWithMethods.class).methodWithMultiValueRequestParams("1", Arrays.asList(3, 7), 5))
+				.withSelfRel();
+
+		UriComponents components = toComponents(link);
+		assertThat(components.getPath(), is("/something/1/foo"));
+
+		MultiValueMap<String, String> queryParams = components.getQueryParams();
+		assertThat(queryParams.get("limit"), contains("5"));
+		assertThat(queryParams.get("items"), containsInAnyOrder("3", "7"));
+	}
+
+	/**
+	 * @see #26, #39
+	 */
+	@Test
+	public void returnsUriComponentsBuilder() {
+
+		UriComponents components = linkTo(PersonController.class).slash("something?foo=bar").toUriComponentsBuilder()
+				.build();
+
+		assertThat(components.getPath(), is("/people/something"));
+		assertThat(components.getQuery(), is("foo=bar"));
+	}
+
+	private static UriComponents toComponents(Link link) {
+		return UriComponentsBuilder.fromUriString(link.getHref()).build();
+	}
+
 	static class Person implements Identifiable<Long> {
 
 		Long id;
@@ -167,6 +239,18 @@ public class ControllerLinkBuilderUnitTest extends TestUtils {
 
 		@RequestMapping("/{id}/foo")
 		HttpEntity<Void> methodWithPathVariable(@PathVariable String id) {
+			return null;
+		}
+
+		@RequestMapping(value = "/{id}/foo")
+		HttpEntity<Void> methodForNextPage(@PathVariable String id, @RequestParam Integer offset,
+				@RequestParam Integer limit) {
+			return null;
+		}
+
+		@RequestMapping(value = "/{id}/foo")
+		HttpEntity<Void> methodWithMultiValueRequestParams(@PathVariable String id, @RequestParam List<Integer> items,
+				@RequestParam Integer limit) {
 			return null;
 		}
 	}

commit 6bc906971ab1fc52b27cdaa9a7fade4d7bb331af

#26, #39 - Fixed potential transitive assertion error in UrComponentsBuilder.
If a String piped into LinkBuilderSupport.slash(…) is empty, the call to UriComponentsBuilder.fromUriString(…) fails with an assertion exception. We now guard against this specific case by returning the current builder instance in case an empty String (potentially returned from the toString() method of the object handed in) would be handed to the UriComponentsBuilder.

The issue was introduced by commit 84efebc7a65afd7b5decbae054af13406ccb59c2.

src/main/java/org/springframework/hateoas/core/LinkBuilderSupport.java
@@ -21,6 +21,7 @@ import org.springframework.hateoas.Identifiable;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.LinkBuilder;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -59,7 +60,13 @@ public abstract class LinkBuilderSupport<T extends LinkBuilder> implements LinkB
 			return slash((Identifiable<?>) object);
 		}
 
-		UriComponents components = UriComponentsBuilder.fromUriString(object.toString()).build();
+		String path = object.toString();
+
+		if (!StringUtils.hasText(path)) {
+			return getThis();
+		}
+
+		UriComponents components = UriComponentsBuilder.fromUriString(path).build();
 		UriComponentsBuilder builder = UriComponentsBuilder.fromUri(uriComponents.toUri());
 
 		for (String pathSegment : components.getPathSegments()) {

src/test/java/org/springframework/hateoas/core/LinkBuilderSupportUnitTest.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.hateoas.core;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.hateoas.TestUtils;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Unit tests for {@link LinkBuilderSupport}.
+ * 
+ * @author Oliver Gierke
+ */
+public class LinkBuilderSupportUnitTest extends TestUtils {
+
+	@Test
+	public void callingSlashWithEmptyStringIsNoOp() {
+
+		SampleLinkBuilder builder = new SampleLinkBuilder(UriComponentsBuilder.newInstance());
+		assertThat(builder.slash(""), is(builder));
+	}
+
+	static class SampleLinkBuilder extends LinkBuilderSupport<SampleLinkBuilder> {
+
+		public SampleLinkBuilder(UriComponentsBuilder builder) {
+			super(builder);
+		}
+
+		@Override
+		protected SampleLinkBuilder getThis() {
+			return this;
+		}
+
+		@Override
+		protected SampleLinkBuilder createNewInstance(UriComponentsBuilder builder) {
+			return new SampleLinkBuilder(builder);
+		}
+	}
+}