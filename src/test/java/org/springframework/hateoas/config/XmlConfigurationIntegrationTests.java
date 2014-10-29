7d0a76a54b3b49e4c3559bae37051d7a08e66c0a
Oliver Gierke <ogierke@gopivotal.com>
2014-10-29 12:53:42 +0100

    #259 - Added test case to show how to use EnableHypermediaSupport from XML.

---
+++ src/test/java/org/springframework/hateoas/config/XmlConfigurationIntegrationTests.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.hateoas.config;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.hateoas.RelProvider;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+
+/**
+ * Integration tests for using {@link EnableHypermediaSupport} from within XML documents.
+ * 
+ * @author Oliver Gierke
+ */
+public class XmlConfigurationIntegrationTests {
+
+	/**
+	 * @see #259
+	 */
+	@Test
+	public void enablesHyperMediaSupportFromXml() {
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml", getClass());
+
+		assertThat(context.getBean(RelProvider.class), is(notNullValue()));
+
+		context.close();
+	}
+
+	@Configuration
+	@EnableHypermediaSupport(type = HypermediaType.HAL)
+	static class HypermediaConfiguration {}
+}
