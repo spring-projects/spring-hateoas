9c4534156429a8081399fe6847b9d5d52781a16a
Oliver Gierke <ogierke@gopivotal.com>
2015-02-26 19:27:42 +0100

    #306 - Introduced TypeReferences helper to create ParameterizedTypeReferences easily.
    TypeReferences contains a set of predefined types that allows to shortcut the creation of ParameterizedTypeReference instances a bit, which is very useful for the usage on the client side (e.g., via RestTemplate).
    
    new ParameterizedTypeReference<Resources<Resource<User>>>() {};
    
    becomes
    
    new TypeReferences.Resources<Resource<User>>() {};
    
    The latter can even be abbreviated by using a static import for TypeReferences.

---
+++ src/test/java/org/springframework/hateoas/mvc/TypeReferencesIntegrationTests.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.hateoas.mvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.client.MockRestServiceServer.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Integration tests for {@link TypeReferences}.
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class TypeReferencesIntegrationTests {
+
+	private static final String USER = "\"firstname\" : \"Dave\", \"lastname\" : \"Matthews\"";
+	private static final String RESOURCE = String.format("{ \"_links\" : { \"self\" : \"/resource\" }, %s }", USER);
+	private static final String RESOURCES_OF_USER = String.format(
+			"{ \"_links\" : { \"self\" : \"/resources\" }, \"_embedded\" : { \"users\" : [ { %s } ] }}", USER);
+	private static final String RESOURCES_OF_RESOURCE = String.format(
+			"{ \"_links\" : { \"self\" : \"/resources\" }, \"_embedded\" : { \"users\" : [ %s ] }}", RESOURCE);
+
+	@Configuration
+	@EnableHypermediaSupport(type = HypermediaType.HAL)
+	static class Config {
+
+		public @Bean RestTemplate template() {
+			return new RestTemplate();
+		}
+	}
+
+	@Autowired RestTemplate template;
+	MockRestServiceServer server;
+
+	@Before
+	public void setUp() {
+		this.server = createServer(template);
+	}
+
+	/**
+	 * @see #306
+	 */
+	@Test
+	public void usesResourceTypeReference() {
+
+		server.expect(requestTo("/resource")).andRespond(withSuccess(RESOURCE, MediaTypes.HAL_JSON));
+
+		ResponseEntity<Resource<User>> response = template.exchange("/resource", HttpMethod.GET, null,
+				new TypeReferences.Resource<User>() {});
+
+		assertExpectedUserResource(response.getBody());
+	}
+
+	/**
+	 * @see #306
+	 */
+	@Test
+	public void usesResourcesTypeReference() {
+
+		server.expect(requestTo("/resources")).andRespond(withSuccess(RESOURCES_OF_USER, MediaTypes.HAL_JSON));
+
+		ResponseEntity<Resources<User>> response = template.exchange("/resources", HttpMethod.GET, null,
+				new TypeReferences.Resources<User>() {});
+		Resources<User> body = response.getBody();
+
+		assertThat(body.hasLink("self"), is(true));
+
+		Collection<User> nested = body.getContent();
+
+		assertThat(nested, hasSize(1));
+		assertExpectedUser(nested.iterator().next());
+	}
+
+	/**
+	 * @see #306
+	 */
+	@Test
+	public void usesResourcesOfResourceTypeReference() {
+
+		server.expect(requestTo("/resources")).andRespond(withSuccess(RESOURCES_OF_RESOURCE, MediaTypes.HAL_JSON));
+
+		ResponseEntity<Resources<Resource<User>>> response = template.exchange("/resources", HttpMethod.GET, null,
+				new TypeReferences.Resources<Resource<User>>() {});
+		Resources<Resource<User>> body = response.getBody();
+
+		assertThat(body.hasLink("self"), is(true));
+
+		Collection<Resource<User>> nested = body.getContent();
+
+		assertThat(nested, hasSize(1));
+		assertExpectedUserResource(nested.iterator().next());
+	}
+
+	private static void assertExpectedUserResource(Resource<User> user) {
+
+		assertThat(user.hasLink("self"), is(true));
+		assertExpectedUser(user.getContent());
+	}
+
+	private static void assertExpectedUser(User user) {
+
+		assertThat(user.firstname, is("Dave"));
+		assertThat(user.lastname, is("Matthews"));
+	}
+
+	static class User {
+		public String firstname, lastname;
+	}
+}
