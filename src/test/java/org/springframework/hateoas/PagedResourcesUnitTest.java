32d7c9bf5194ac5a70ef47ee88adcbfab8621725
Oliver Gierke <ogierke@gopivotal.com>
2015-02-27 14:56:31 +0100

    #309 - Fixed calculation of total pages in PageMetadata.

--- src/test/java/org/springframework/hateoas/PagedResourcesUnitTest.java
+++ src/test/java/org/springframework/hateoas/PagedResourcesUnitTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -94,5 +94,13 @@ public class PagedResourcesUnitTest {
 	@Test
 	public void allowsOneIndexedPages() {
 		new PageMetadata(10, 1, 0);
+	}
+
+	/**
+	 * @see #309
+	 */
+	@Test
+	public void calculatesTotalPagesCorrectly() {
+		assertThat(new PageMetadata(5, 0, 16).getTotalPages(), is(4L));
 	}
 }
