spring-projects/spring-hateoas

commit 673f82d38878fab295431ef51903ccee5c6986d4

#192 - Added linkTo(Class<?>, Method, Object...) to MethodLinkBuilderFactory.
The newly introduced method is needed to make sure the correct mappings are looked up if the method given is actually not declared on the class the method shall eventually be invoked on.

This is basically allowing to try to build links, similar to the dummy invocation mechanism but effectively avoiding the overhead of creating such invocation manually (as it would usually require reflection to be used).

src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilder.java
@@ -88,10 +88,24 @@ public class ControllerLinkBuilder extends LinkBuilderSupport<ControllerLinkBuil
 		return builder.slash(expandedComponents);
 	}
 
+	/*
+	 * @see org.springframework.hateoas.MethodLinkBuilderFactory#linkTo(Method, Object...)
+	 */
 	public static ControllerLinkBuilder linkTo(Method method, Object... parameters) {
+		return linkTo(method.getDeclaringClass(), method);
+	}
 
-		UriTemplate template = new UriTemplate(DISCOVERER.getMapping(method));
+	/*
+	 * @see org.springframework.hateoas.MethodLinkBuilderFactory#linkTo(Class<?>, Method, Object...)
+	 */
+	public static ControllerLinkBuilder linkTo(Class<?> controller, Method method, Object... parameters) {
+
+		Assert.notNull(controller, "Controller type must not be null!");
+		Assert.notNull(method, "Method must not be null!");
+
+		UriTemplate template = new UriTemplate(DISCOVERER.getMapping(controller, method));
 		URI uri = template.expand(parameters);
+
 		return new ControllerLinkBuilder(getBuilder()).slash(uri);
 	}
 

src/main/java/org/springframework/hateoas/MethodLinkBuilderFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,9 +39,20 @@ public interface MethodLinkBuilderFactory<T extends LinkBuilder> extends LinkBui
 	T linkTo(Method method, Object... parameters);
 
 	/**
+	 * Returns a {@link LinkBuilder} pointing to the URI mapped to the given {@link Method} assuming it was invoked on an
+	 * object of the given type.
+	 * 
+	 * @param type must not be {@literal null}.
+	 * @param method must not be {@literal null}.
+	 * @param parameters
+	 * @return
+	 */
+	T linkTo(Class<?> type, Method method, Object... parameters);
+
+	/**
 	 * Returns a {@link LinkBuilder} pointing to the URI mapped to the method the result is handed into this method. Use
-	 * {@link DummyInvocationUtils#methodOn(Class, Object...)} to obtain a dummy instance of a controller to record a dummy
-	 * method invocation on. See {@link ControllerLinkBuilder#linkTo(Object)} for an example.
+	 * {@link DummyInvocationUtils#methodOn(Class, Object...)} to obtain a dummy instance of a controller to record a
+	 * dummy method invocation on. See {@link ControllerLinkBuilder#linkTo(Object)} for an example.
 	 * 
 	 * @see ControllerLinkBuilder#linkTo(Object)
 	 * @param methodInvocationResult must not be {@literal null}.

src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilderFactory.java
@@ -92,6 +92,15 @@ public class ControllerLinkBuilderFactory implements MethodLinkBuilderFactory<Co
 
 	/* 
 	 * (non-Javadoc)
+	 * @see org.springframework.hateoas.MethodLinkBuilderFactory#linkTo(java.lang.Class, java.lang.reflect.Method, java.lang.Object[])
+	 */
+	@Override
+	public ControllerLinkBuilder linkTo(Class<?> controller, Method method, Object... parameters) {
+		return ControllerLinkBuilder.linkTo(controller, method, parameters);
+	}
+
+	/* 
+	 * (non-Javadoc)
 	 * @see org.springframework.hateoas.MethodLinkBuilderFactory#linkTo(java.lang.Object)
 	 */
 	@Override

src/test/java/org/springframework/hateoas/mvc/ControllerLinkBuilderUnitTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
@@ -362,6 +363,18 @@ public class ControllerLinkBuilderUnitTest extends TestUtils {
 	public void usesRootMappingOfTargetClassForMethodsOfParentClass() {
 
 		Link link = linkTo(methodOn(ChildControllerWithRootMapping.class).someEmptyMappedMethod()).withSelfRel();
+		assertThat(link.getHref(), endsWith("/root"));
+	}
+
+	/**
+	 * @see #192
+	 */
+	@Test
+	public void usesRootMappingOfTargetClassForMethodsOfParen() throws Exception {
+
+		Method method = ParentControllerWithoutRootMapping.class.getMethod("someEmptyMappedMethod");
+
+		Link link = linkTo(ChildControllerWithRootMapping.class, method).withSelfRel();
 		assertThat(link.getHref(), endsWith("/root"));
 	}
 

commit 1ae44c1896e660df9a1d6565418afa08f7d1f982

#237 - Added missing variable in call to ControllerLinkBuilder.linkTo(…).
673f82d introduced a glitch in a delegation within ControllerLinkBuilder.linkTo(Class<?> controller, Method method, Object... parameters) which is fixed by this commit.

Original pull request: #300.

src/main/java/org/springframework/hateoas/mvc/ControllerLinkBuilder.java
@@ -93,7 +93,7 @@ public class ControllerLinkBuilder extends LinkBuilderSupport<ControllerLinkBuil
 	 * @see org.springframework.hateoas.MethodLinkBuilderFactory#linkTo(Method, Object...)
 	 */
 	public static ControllerLinkBuilder linkTo(Method method, Object... parameters) {
-		return linkTo(method.getDeclaringClass(), method);
+		return linkTo(method.getDeclaringClass(), method, parameters);
 	}
 
 	/*