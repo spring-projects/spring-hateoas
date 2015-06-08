fb94da5fad5d616566ef1444acce2b789d4db29d
Maxime Falaize <maxime.falaize@gmail.com>
2015-06-08 14:14:17 +0200

    Adding test cases for an Id Simple Pojo.

---
+++ src/test/java/org/springframework/hateoas/hal/IdSimplePojo.java
@@ -0,0 +1,14 @@
+package org.springframework.hateoas.hal;
+
+public class IdSimplePojo extends SimplePojo {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
