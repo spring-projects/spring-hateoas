31029a855d86d34d251e9dac7e464b3cb153ae9b
igor <igor@hdivsecurity.com>
2016-04-21 10:25:04 +0200

    Remove last comma reducing StringBuilder length

--- src/main/java/org/springframework/hateoas/forms/Template.java
+++ src/main/java/org/springframework/hateoas/forms/Template.java
@@ -92,8 +92,8 @@ public class Template extends Link {
 		for (RequestMethod rm : method) {
 			sb.append(rm.toString()).append(',');
 		}
-		String methodStr = sb.toString();
-		return methodStr.substring(0, methodStr.length() - 1);
+		sb.setLength(sb.length() - 1);
+		return sb.toString();
 	}
 
 }
