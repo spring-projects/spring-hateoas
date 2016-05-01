dd1acc5629656c85daf3fd95db4b07b0445afe5b
Dietrich Schulten <ds@escalon.de>
2016-05-01 15:13:23 +0200

    Removed obsolete AnnotatedParameter and AnnotatedParameters.java

--- src/main/java/de/escalon/hypermedia/affordance/AnnotatedParameters.java
+++
@@ -1,9 +0,0 @@
-package de.escalon.hypermedia.affordance;
-
-/**
- * Represents a set of named annotated parameters. Created by Dietrich on 17.05.2015.
- */
-public interface AnnotatedParameters {
-
-	AnnotatedParameter getAnnotatedParameter(String name);
-}
