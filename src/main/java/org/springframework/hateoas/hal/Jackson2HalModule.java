469457f1d46f557b91664af99af1a6070c86707e
Karl the Pagan <karlthepagan@gmail.com>
2016-08-10 09:20:23 -0700

    #480 relax EmbeddedMapper to public access

--- src/main/java/org/springframework/hateoas/hal/Jackson2HalModule.java
+++ src/main/java/org/springframework/hateoas/hal/Jackson2HalModule.java
@@ -872,7 +872,7 @@ public class Jackson2HalModule extends SimpleModule {
 	 *
 	 * @author Oliver Gierke
 	 */
-	private static class EmbeddedMapper {
+	public static class EmbeddedMapper {
 
 		private RelProvider relProvider;
 		private CurieProvider curieProvider;
