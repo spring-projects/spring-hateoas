05550952d12cb709d4f3651dc393cfee51874019
Kulcsár Roland <roolandeu@gmail.com>
2017-05-08 20:06:35 +0200

    #580 - Update IANA relation types
    Per https://www.iana.org/assignments/link-relations/link-relations.xhtml, the list of standardized IANA rels has grown.
    
    Original pull-request: #580

--- src/main/java/org/springframework/hateoas/IanaRels.java
+++ src/main/java/org/springframework/hateoas/IanaRels.java
@@ -27,6 +27,7 @@ import java.util.HashSet;
  * 
  * @see http://www.iana.org/assignments/link-relations/link-relations.xhtml
  * @author Oliver Gierke
+ * @author Roland Kulcsár
  */
 @UtilityClass
 public class IanaRels {
@@ -37,15 +38,16 @@ public class IanaRels {
 
 		Collection<String> rels = new HashSet<String>();
 
-		rels.addAll(Arrays.asList("about", "alternate", "appendix", "archives", "author", "bookmark", "canonical",
-				"chapter", "collection", "contents", "copyright", "create-form", "current", "describedby", "describes",
-				"disclosure", "duplicate", "edit", "edit-form", "edit-media", "enclosure", "first", "glossary", "help", "hosts",
-				"hub", "icon", "index", "item", "last", "latest-version", "license", "lrdd", "memento", "monitor",
-				"monitor-group", "next", "next-archive", "nofollow", "noreferrer", "original", "payment", "predecessor-version",
-				"prefetch", "prev", "preview", "previous", "prev-archive", "privacy-policy", "profile", "related", "replies",
+		rels.addAll(Arrays.asList("about", "alternate", "appendix", "archives", "author", "blocked-by", "bookmark",
+				"canonical", "chapter", "collection", "contents", "convertedFrom", "copyright", "create-form", "current",
+				"describedby", "describes", "disclosure", "dns-prefetch", "duplicate", "edit", "edit-form", "edit-media",
+				"enclosure", "first", "glossary", "help", "hosts", "hub", "icon", "index", "item", "last", "latest-version",
+				"license", "lrdd", "memento", "monitor", "monitor-group", "next", "next-archive", "nofollow", "noreferrer",
+				"original", "payment", "pingback", "preconnect", "predecessor-version", "prefetch", "preload", "prerender",
+				"prev", "preview", "previous", "prev-archive", "privacy-policy", "profile", "related", "restconf", "replies",
 				"search", "section", "self", "service", "start", "stylesheet", "subsection", "successor-version", "tag",
-				"terms-of-service", "timegate", "timemap", "type", "up", "version-history", "via", "working-copy",
-				"working-copy-of"));
+				"terms-of-service", "timegate", "timemap", "type", "up", "version-history", "via", "webmention",
+				"working-copy", "working-copy-of"));
 
 		RELS = Collections.unmodifiableCollection(rels);
 	}
