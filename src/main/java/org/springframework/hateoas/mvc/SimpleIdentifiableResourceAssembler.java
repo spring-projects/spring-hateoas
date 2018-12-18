3298119ba229bbec4bd0e0228e1d7eca958f89cf
Greg Turnquist <gturnquist@pivotal.io>
2018-12-18 11:03:50 -0600

    Create SimpleResourceAssembler based on Identifiable

---
+++ src/main/java/org/springframework/hateoas/mvc/SimpleIdentifiableResourceAssembler.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.hateoas.mvc;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.core.GenericTypeResolver;
+import org.springframework.hateoas.Identifiable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.LinkBuilder;
+import org.springframework.hateoas.RelProvider;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.SimpleResourceAssembler;
+import org.springframework.hateoas.core.EvoInflectorRelProvider;
+
+/**
+ * Using a Spring MVC-based {@link ControllerLinkBuilder} and an {@link Identifiable} domain object, construct
+ * a {@link SimpleResourceAssembler}.
+ *
+ * @author Greg Turnquist
+ */
+public class SimpleIdentifiableResourceAssembler<T extends Identifiable<?>> implements SimpleResourceAssembler<T> {
+
+	/**
+	 * The Spring MVC class for the {@link Identifiable} from which links will be built.
+	 */
+	private final Class<?> controllerClass;
+
+	/**
+	 * A {@link RelProvider} to look up names of links as options for resource paths.
+	 */
+	private final RelProvider relProvider;
+
+	/**
+	 * A {@link Class} depicting the {@link Identifiable}'s type.
+	 */
+	private final Class<?> resourceType;
+
+	/**
+	 * Default base path as empty.
+	 */
+	private String basePath = "";
+
+	/**
+	 * Default assembler based on a Spring MVC controller, resource type, and {@link RelProvider}. With this combination
+	 * of information, resources can be defined.
+	 *
+	 * @see #setBasePath(String) to adjust base path to something like "/api"/
+	 *
+	 * @param controllerClass - Spring MVC controller to base links off of
+	 * @param relProvider - generates the links items
+	 */
+	public SimpleIdentifiableResourceAssembler(Class<?> controllerClass, RelProvider relProvider) {
+
+		this.controllerClass = controllerClass;
+		this.relProvider = relProvider;
+
+		// Find the "T" type contained in "T extends Identifiable<?>", e.g. SimpleIdentifiableResourceAssembler<User> -> User
+		this.resourceType = GenericTypeResolver.resolveTypeArgument(this.getClass(), SimpleIdentifiableResourceAssembler.class);
+	}
+
+	/**
+	 * Alternate constructor that falls back to {@link EvoInflectorRelProvider}.
+	 * 
+	 * @param controllerClass
+	 */
+	public SimpleIdentifiableResourceAssembler(Class<?> controllerClass) {
+		this(controllerClass, new EvoInflectorRelProvider());
+	}
+
+	/**
+	 * Define links to add to every {@link Resource}.
+	 *
+	 * @param resource
+	 */
+	@Override
+	public void addLinks(Resource<T> resource) {
+
+		Link resourceSelfLink = getCollectionLinkBuilder().slash(resource.getContent()).withSelfRel();
+		Link resourceCollectionLink = getCollectionLinkBuilder().withRel(this.relProvider.getCollectionResourceRelFor(this.resourceType));
+
+		resource.add(resourceSelfLink, resourceCollectionLink);
+	}
+
+	/**
+	 * Define links to add to {@link Resources} collection.
+	 *
+	 * @param resources
+	 */
+	@Override
+	public void addLinks(Resources<Resource<T>> resources) {
+
+		Link resourceCollectionLink = getCollectionLinkBuilder().withSelfRel();
+		resources.add(resourceCollectionLink);
+	}
+
+	/**
+	 * Build up a URI for the collection using the Spring MVC controller followed by the resource type transformed
+	 * by the {@link RelProvider}.
+	 *
+	 * Assumption is that a {@literal EmployeeController} serving up {@literal Employee}
+	 * objects will be serving resources at {@code /employees} and {@code /employees/1} respectively.
+	 *
+	 * If this is not the case, simply override this method in your concrete instance, or simply resort to
+	 * overriding {@link #addLinks(Resource)} and {@link #addLinks(Resources)} where you have full control over exactly
+	 * what links are put in the individual and collection resources.
+	 *
+	 * @return
+	 */
+	protected LinkBuilder getCollectionLinkBuilder() {
+
+		String[] pathComponents = (getPrefix() + this.relProvider.getCollectionResourceRelFor(this.resourceType)).split("/");
+
+		ControllerLinkBuilder linkBuilder = linkTo(this.controllerClass);
+
+		for (String pathComponent : pathComponents) {
+			if (!pathComponent.isEmpty()) {
+				linkBuilder = linkBuilder.slash(pathComponent);
+			}
+		}
+
+		return linkBuilder;
+	}
+
+	/**
+	 * Construct the prefix to a resource, with {@literal "/"} being the basis for no prefix at all.
+	 *
+	 * @return
+	 */
+	private String getPrefix() {
+		return getBasePath().isEmpty() ? "" : getBasePath() + "/";
+	}
+
+	public String getBasePath() {
+		return this.basePath;
+	}
+
+	public void setBasePath(String basePath) {
+		this.basePath = basePath;
+	}
+}
---
+++ src/test/java/org/springframework/hateoas/mvc/SimpleIdentifiableResourceAssemblerTest.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.hateoas.mvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.hateoas.Identifiable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.core.EvoInflectorRelProvider;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * @author Greg Turnquist
+ */
+public class SimpleIdentifiableResourceAssemblerTest {
+
+	MockHttpServletRequest request;
+
+	@Before
+	public void setUp() {
+
+		request = new MockHttpServletRequest();
+		ServletRequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		RequestContextHolder.setRequestAttributes(requestAttributes);
+	}
+
+	@Test
+	public void convertingToResourceShouldWork() {
+
+		TestResourceAssembler assembler = new TestResourceAssembler();
+		Resource<Employee> resource = assembler.toResource(new Employee(1, "Frodo"));
+
+		assertThat(resource.getContent().getName()).isEqualTo("Frodo");
+	}
+
+	@Test
+	public void convertingToResourcesShouldWork() {
+
+		// given
+		TestResourceAssembler assembler = new TestResourceAssembler();
+
+		Employee frodo = new Employee(1, "Frodo");
+		Resource<Employee> frodoResource = new Resource<>(
+			frodo,
+			new Link("http://localhost/employees/1").withSelfRel(),
+			new Link("http://localhost/employees").withRel("employees"));
+
+		// when
+		Resources<Resource<Employee>> resources = assembler.toResources(Arrays.asList(frodo));
+
+		// then
+		assertThat(resources.getContent()).hasSize(1);
+		assertThat(resources.getContent()).contains(frodoResource);
+		assertThat(resources.getContent().iterator().next()).isEqualTo(frodoResource);
+		assertThat(resources.getLinks()).hasSize(1);
+		assertThat(resources.getLinks()).containsExactly(new Link("http://localhost/employees").withSelfRel());
+	}
+
+	@Test
+	public void convertingToResourceWithCustomLinksShouldWork() {
+
+		ResourceAssemblerWithCustomLink assembler = new ResourceAssemblerWithCustomLink();
+		Employee frodo = new Employee(1, "Frodo");
+		Resource<Employee> resource = assembler.toResource(frodo);
+
+		assertThat(resource.getContent().getName()).isEqualTo("Frodo");
+		assertThat(resource.getLinks()).hasSize(1);
+		assertThat(resource.getLinks()).containsExactly(new Link("/employees").withRel("employees"));
+	}
+
+	@Test
+	public void convertingToResourcesWithCustomLinksShouldWork() {
+
+		// given
+		ResourceAssemblerWithCustomLink assembler = new ResourceAssemblerWithCustomLink();
+
+		Employee frodo = new Employee(1, "Frodo");
+		Resource<Employee> frodoResource = new Resource<>(frodo, new Link("/employees").withRel("employees"));
+
+		// when
+		Resources<Resource<Employee>> resources = assembler.toResources(Arrays.asList(frodo));
+
+		// then
+		assertThat(resources.getContent()).hasSize(1);
+		assertThat(resources.getContent()).containsExactly(frodoResource);
+		assertThat(resources.getContent().iterator().next()).isEqualTo(frodoResource);
+		assertThat(resources.getLinks()).hasSize(1);
+		assertThat(resources.getLinks()).containsExactly(new Link("/allEmployees").withRel("all-employees"));
+	}
+
+	@Test
+	public void changingBasePathShouldWorkForResourceAndResources() {
+
+		// given
+		TestResourceAssembler assembler = new TestResourceAssembler();
+		assembler.setBasePath("/api");
+
+		Employee frodo = new Employee(1, "Frodo");
+		Resource<Employee> expectedEmployeeResource = new Resource<>(
+			frodo,
+			new Link("http://localhost/api/employees/1").withSelfRel(),
+			new Link("http://localhost/api/employees").withRel("employees"));
+
+		// when
+		Resource<Employee> actualEmployeeResource = assembler.toResource(frodo);
+
+		// then
+		assertThat(actualEmployeeResource).isEqualTo(expectedEmployeeResource);
+
+		// given
+		Resources<Resource<Employee>> expectedEmployeeResources = new Resources<>(Arrays.asList(expectedEmployeeResource));
+		expectedEmployeeResources.add(new Link("http://localhost/api/employees").withSelfRel());
+
+		// when
+		Resources<Resource<Employee>> actualEmployeeResources = assembler.toResources(Arrays.asList(frodo));
+
+		// then
+		assertThat(actualEmployeeResources).isEqualTo(expectedEmployeeResources);
+	}
+	
+	class TestResourceAssembler extends SimpleIdentifiableResourceAssembler<Employee> {
+		
+		TestResourceAssembler() {
+			super(EmployeeController.class, new EvoInflectorRelProvider());
+		}
+	}
+
+	class ResourceAssemblerWithCustomLink extends SimpleIdentifiableResourceAssembler<Employee> {
+
+		ResourceAssemblerWithCustomLink() {
+			super(EmployeeController.class, new EvoInflectorRelProvider());
+		}
+
+		@Override
+		public void addLinks(Resource<Employee> resource) {
+			resource.add(new Link("/employees").withRel("employees"));
+		}
+
+		@Override
+		public void addLinks(Resources<Resource<Employee>> resources) {
+			resources.add(new Link("/allEmployees").withRel("all-employees"));
+		}
+	}
+
+	@Data
+	static class Employee implements Identifiable<Integer> {
+		private final Integer id;
+		private final String name;
+
+		@Override
+		public Optional<Integer> getId() {
+			return Optional.ofNullable(this.id);
+		}
+	}
+	
+	@Controller
+	static class EmployeeController {
+		
+		final static Map<Integer, Employee> EMPLOYEES = new HashMap<Integer, Employee>() {{
+			put(1, new Employee(1, "Frodo"));
+			put(2, new Employee(2, "Bilbo"));
+		}};
+		
+		@GetMapping("/employees")
+		Collection<Employee> employees() {
+			return EMPLOYEES.values();
+		}
+		
+		@GetMapping("/employees/{id}")
+		Employee employee(@PathVariable int id) {
+			return EMPLOYEES.get(id);
+		}
+	}
+
+}
