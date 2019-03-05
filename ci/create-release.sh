e613a230c83893413dadfb7ae8387def1e249caa
Greg Turnquist <gturnquist@pivotal.io>
2019-03-05 10:12:33 -0600

    #853 - Polishing.

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -9,14 +9,14 @@ SNAPSHOT=$3
 # Bump up the version in pom.xml to the desired version and commit the change
 ./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
-git commit --message "$ISSUE - Releasing Spring HATEOAS v$RELEASE."
+git commit --message "$ISSUE - Releasing Spring HATEOAS $RELEASE."
 
 # Tag the release
-git tag -s v$RELEASE -m "v$RELEASE"
+git tag -s $RELEASE -m "$RELEASE"
 
 # Bump up the version in pom.xml to the next snapshot
 ./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
 git add .
-git commit --message "$ISSUE - Continue development on v$SNAPSHOT."
+git commit --message "$ISSUE - Continue development on $SNAPSHOT."
 
 
