3145f67fb4700174b93159b64f887af303e617fe
Greg Turnquist <gturnquist@pivotal.io>
2019-03-05 09:26:52 -0600

    #853 - Fine tune release script to allow ticket number.

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 
-RELEASE=$1
-SNAPSHOT=$2
+TICKET=$1
+RELEASE=$2
+SNAPSHOT=$3
 
 # Bump up the version in pom.xml to the desired version and commit the change
 ./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
-git commit --message "Releasing Spring HATEOAS v$RELEASE"
+git commit --message "$ISSUE - Releasing Spring HATEOAS v$RELEASE."
 
 # Tag the release
 git tag -s v$RELEASE -m "v$RELEASE"
@@ -16,6 +17,6 @@ git tag -s v$RELEASE -m "v$RELEASE"
 # Bump up the version in pom.xml to the next snapshot
 ./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
 git add .
-git commit --message "Continue development on v$SNAPSHOT"
+git commit --message "$ISSUE - Continue development on v$SNAPSHOT."
 
 
