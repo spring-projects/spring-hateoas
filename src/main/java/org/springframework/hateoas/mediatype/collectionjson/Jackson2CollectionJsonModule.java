5bfa1eb2db46fdd7a8c46e1b099fc75adab378d6
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-21 15:09:36 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 2 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 268 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).

--- src/main/java/org/springframework/hateoas/mediatype/collectionjson/Jackson2CollectionJsonModule.java
+++ src/main/java/org/springframework/hateoas/mediatype/collectionjson/Jackson2CollectionJsonModule.java
@@ -5,7 +5,7 @@ stand bisher hauptsächlich darin, dass  * Copyright 2015-2019 the original auth
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
