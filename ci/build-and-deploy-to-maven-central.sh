9073286b6f2237b5572dc49b8b1523d13f281434
Greg L. Turnquist <gturnquist@vmware.com>
2020-12-18 09:10:33 -0600

    #1420 - Backport directly releasing to maven central to 1.1.x.
    By removing our need to use bintray for releases and instead deploying directly to maven central, we can reduce the risk of issues.
    
    Related issue: #1417, #1419

---
+++ ci/build-and-deploy-to-maven-central.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+PROJECT_VERSION=$1
+
+#
+# Stage on Maven Central
+#
+echo 'Staging on Maven Central...'
+
+GNUPGHOME=/tmp/gpghome
+export GNUPGHOME
+
+mkdir $GNUPGHOME
+cp $KEYRING $GNUPGHOME
+
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw \
+    -s settings.xml \
+    -P${PROFILE} \
+    -Dmaven.test.skip=true \
+    -Dgpg.passphrase=${PASSPHRASE} \
+    -Dgpg.secretKeyring=${GNUPGHOME}/secring.gpg \
+    -DstagingDescription="Releasing ${PROJECT_VERSION}" \
+    clean deploy -B
