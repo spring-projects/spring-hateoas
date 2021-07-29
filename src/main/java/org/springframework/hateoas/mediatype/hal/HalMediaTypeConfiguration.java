748da756db90edb4c3bf6d90193c3e8794ce1ac3
Oliver Drotbohm <odrotbohm@vmware.com>
2021-07-29 17:57:36 +0200

    #1591 - Polishing.
    Port configuration lookup caching from HAL FORMS into HAL configuration.

--- src/main/java/org/springframework/hateoas/mediatype/hal/HalMediaTypeConfiguration.java
+++ src/main/java/org/springframework/hateoas/mediatype/hal/HalMediaTypeConfiguration.java
@@ -46,6 +46,8 @@ public class HalMediaTypeConfiguration implements HypermediaMappingInformation {
 	private final @Qualifier("messageResolver") MessageResolver resolver;
 	private final AutowireCapableBeanFactory beanFactory;
 
+	private HalConfiguration resolvedConfiguration;
+
 	public HalMediaTypeConfiguration(LinkRelationProvider relProvider, ObjectProvider<CurieProvider> curieProvider,
 			ObjectProvider<HalConfiguration> halConfiguration, MessageResolver resolver,
 			AutowireCapableBeanFactory beanFactory) {
@@ -68,7 +70,7 @@ public class HalMediaTypeConfiguration implements HypermediaMappingInformation {
 	 */
 	@Override
 	public List<MediaType> getMediaTypes() {
-		return this.halConfiguration.getIfAvailable(HalConfiguration::new).getMediaTypes();
+		return getResolvedConfiguration().getMediaTypes();
 	}
 
 	/*
@@ -78,7 +80,7 @@ public class HalMediaTypeConfiguration implements HypermediaMappingInformation {
 	@Override
 	public ObjectMapper configureObjectMapper(ObjectMapper mapper) {
 
-		HalConfiguration halConfiguration = this.halConfiguration.getIfAvailable(HalConfiguration::new);
+		HalConfiguration halConfiguration = getResolvedConfiguration();
 
 		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		mapper.registerModule(new Jackson2HalModule());
@@ -88,5 +90,19 @@ public class HalMediaTypeConfiguration implements HypermediaMappingInformation {
 		halConfiguration.customize(mapper);
 
 		return mapper;
+	}
+
+	/**
+	 * Lookup and cache the {@link HalConfiguration} instance to be used.
+	 *
+	 * @return will never be {@literal null}.
+	 */
+	private HalConfiguration getResolvedConfiguration() {
+
+		if (resolvedConfiguration == null) {
+			this.resolvedConfiguration = halConfiguration.getIfAvailable(HalConfiguration::new);
+		}
+
+		return resolvedConfiguration;
 	}
 }
