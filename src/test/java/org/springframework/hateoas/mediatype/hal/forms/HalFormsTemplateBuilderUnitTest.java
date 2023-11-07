688484610cb9ecf6ee24abae611997fc3b51f0c2
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-11-07 14:37:33 +0100

    Support for HAL-FORMS value element

--- src/test/java/org/springframework/hateoas/mediatype/hal/forms/HalFormsTemplateBuilderUnitTest.java
+++ src/test/java/org/springframework/hateoas/mediatype/hal/forms/HalFormsTemplateBuilderUnitTest.java
@@ -201,6 +201,28 @@ class HalFormsTemplateBuilderUnitTest {
 				});
 	}
 
+	@Test
+	void rendersValues() {
+		String value = "1234123412341234";
+
+		HalFormsConfiguration configuration = new HalFormsConfiguration() //
+				.withValues(PatternExample.class, "number", metadata -> value);
+
+		RepresentationModel<?> models = new RepresentationModel<>(
+				Affordances.of(Link.of("/example", LinkRelation.of("create"))) //
+						.afford(HttpMethod.POST) //
+						.withInput(PatternExample.class) //
+						.toLink());
+
+		Map<String, HalFormsTemplate> templates = new HalFormsTemplateBuilder(configuration, MessageResolver.DEFAULTS_ONLY)
+				.findTemplates(models);
+
+		assertThat(templates.get("default").getPropertyByName("number")) //
+				.hasValueSatisfying(it -> {
+					assertThat(it.getValue()).isEqualTo(value);
+				});
+	}
+
 	@Test // #1510
 	void propagatesSelectedValueToProperty() {
 
