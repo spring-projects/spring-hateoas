3ecccabd492838ae5934fe9cfbfefeb8f7d7f58b
simhani1 <simhani1@gmail.com>
2026-01-16 19:42:35 +0900

    chore: update license headers and Javadoc author
    Signed-off-by: simhani1 <simhani1@gmail.com>

--- src/main/java/org/springframework/hateoas/server/core/MethodParameters.java
+++ src/main/java/org/springframework/hateoas/server/core/MethodParameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ import org.springframework.util.ConcurrentReferenceHashMap;
  * Value object to represent {@link MethodParameters} to allow to easily find the ones with a given annotation.
  *
  * @author Oliver Gierke
+ * @author Jonghan Sim
  */
 public class MethodParameters {
 
